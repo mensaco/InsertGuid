a72225c35dfe115d70eb11fac290b9e89ef484d8
Mensur Islami <mi@live.de>
2022-02-26 19:05:23 +0100

    first commit

---
+++ InsertGuid/Commands/MyCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace InsertGuid
+{
+    [Command(PackageIds.MyInsertGuidCommand)]
+    internal sealed class MyInsertGuidCommand : BaseCommand<MyInsertGuidCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selections = docView?.TextView.Selection.SelectedSpans;
+
+            for (int i = selections.Count - 1; i >= 0; i--)
+            {
+                var selection = selections[i];
+                docView.TextBuffer.Replace(selection, Guid.NewGuid().ToString());
+            }
+        }
+    }
+}
